fcf34ac4bce62f2131ae0a18260178b71c8cb9da
ashwini-kemshetty <44300238+ashwini-kemshetty@users.noreply.github.com>
2020-04-04 14:20:52 +0530

    changes to the data

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def predict():
     data = request.get_json()
-    tranformed_data = tf_idf_load.transform(data["data"])
+    print(data)
+    tranformed_data = tf_idf_load.transform([data["data"]])
     prediction = np.array2string(classifier.predict(tranformed_data)[0])
     probability = np.array2string(calibrated.predict_proba(tranformed_data)[:,1][0])
     return jsonify({"polarity" : prediction, "probability": probability})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
-
